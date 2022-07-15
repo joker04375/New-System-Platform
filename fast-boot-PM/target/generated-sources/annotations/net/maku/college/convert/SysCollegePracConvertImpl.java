89b645a281831116ebb0daa6c5f4b682870e2f1c
leo <995604410@qq.com>
2022-07-15 23:33:54 +0800

    edit by lzm 7/14

---
+++ fast-boot-PM/target/generated-sources/annotations/net/maku/college/convert/SysCollegePracConvertImpl.java
@@ -0,0 +1,11 @@
+package net.maku.college.convert;
+
+import javax.annotation.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-07-14T23:18:56+0800",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 1.8.0_322 (Amazon.com Inc.)"
+)
+public class SysCollegePracConvertImpl implements SysCollegePracConvert {
+}
